61d5cac4482dfd6ade890418c61c0d674aba3f93
User <user.push@hackerrank.workspace>
2022-01-17 01:40:25 +0000

    Auto save changes
    Commit by HackerRank

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,9 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
-
+router.get('/', function(req, res, next) {
+  res.send('<p>HTML Data</p>');
+});
 
 module.exports = router;
 
