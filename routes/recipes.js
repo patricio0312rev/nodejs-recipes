9b01f0f678ea21015958347af7b787023c3dc60f
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 21:25:24 -0500

    Validando y obteniendo el recipe

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,15 @@ var router = require('express').Router();
 
 
 router.get('/step/:id', (req, res) => {
-  
+  const { id } = req.params;
+
+  // Obteniendo el recipe
+  const recipe = recipes[+id - 1];
+
+  // Validando si se encontró o no
+  if (!recipe) {
+    return res.status(400).send("NOT_FOUND");
+  }
 });
 module.exports = router;
 
